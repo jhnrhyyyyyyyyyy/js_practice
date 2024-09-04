f2ddce030e2e7ed4fb24923cf27e17043b6a899d
jhnrhyyyyyyyyyy <jhnrhyyyyyyyyyy@gmail.com>
2024-09-04 18:03:34 +0800

    Complete Variable Section

---
+++ app.js
@@ -0,0 +1,34 @@
+console.log("Hello World!");
+let myname = ("John Rhay Domingo");
+console.log('My name is ' + myname);
+
+console.log("Variables");
+let name = ("Quiboloy");
+let age = 1000;
+console.log("Name:" , name);
+console.log("Age:" , age , "years old");
+
+console.log("Numbers");
+let num1 = 20;
+let num2 = 5;
+
+let sum = num1 + num2;
+
+console.log("The sum of", num1, "and", num2, "is", sum);
+
+console.log("Booleans");
+let isRaining = true;
+console.log("Its been raining in Candon?", isRaining);
+
+console.log("Comparison Operators");
+let x = 1;
+let y = 2;
+
+if (x == y) {
+    console.log(x,"and", y, "is equal");
+} else {
+    console.log(x,"and", y, "is not equal");
+}
+
+
+
